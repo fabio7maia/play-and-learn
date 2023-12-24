f8e400648e66e96947e24fa36d4696ef2e57646c
Fábio Maia <fabio.maia@itsector.pt>
2023-12-24 00:09:22 +0000

    feat: applied some improves in game

--- src/components/gameBlock.tsx
+++ src/components/gameBlock.tsx
@@ -1,44 +1,282 @@
+import {
+  ClockIcon,
+  GiftIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/24/solid";
 import React from "react";
-import { AGE_VALUES, LEVEL_VALUES, storage } from "../lib/storage";
+import { storage } from "../lib/storage";
+
+type GameState = {
+  questionIndex: number;
+  points: number;
+  timer: number;
+  status:
+    | "none"
+    | "loading"
+    | "readyToStart"
+    | "inProgress"
+    | "ended"
+    | "error";
+  questions: { question: string; answer: string; choices: string[] }[];
+};
 
 export const GameBlock: React.FC = () => {
   const { settings } = storage.get();
-  const [formData, setFormData] = React.useState(settings);
+  const intervalId = React.useRef<number>();
+  const gameState = React.useRef<GameState>({
+    questionIndex: 0,
+    points: 0,
+    timer: 10,
+    status: "loading",
+    questions: [],
+  });
+  const [_, setTick] = React.useState(0);
 
-  const onChangeSelect = (evt: React.ChangeEvent<HTMLSelectElement>) => {
-    setFormData((formData) => ({
-      ...formData,
-      [evt.target.name]: evt.target.value,
-    }));
+  const initializeGame = () => {
+    const body = new FormData();
+    body.append("age", settings.age);
+    body.append("level", settings.level);
+    body.append("theme", settings.theme);
+
+    gameState.current.points = 0;
+    gameState.current.questionIndex = 0;
+    gameState.current.questions = [];
+    gameState.current.status = "loading";
+    gameState.current.timer = 10;
+    update();
+
+    fetch("/api/questions", {
+      method: "POST",
+      body,
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        gameState.current.questions = [
+          {
+            answer: "1",
+            choices: ["1", "2", "3", "4"],
+            question: "Qual?",
+          },
+        ];
+        gameState.current.status = "readyToStart";
+
+        update();
+      })
+      .catch((err) => {
+        gameState.current.status = "error";
+
+        update();
+      });
   };
 
-  const onClickSave = () => {
-    console.log("SettingsCard", { formData });
-    storage.set({ settings: formData });
+  React.useEffect(() => {
+    initializeGame();
+
+    intervalId.current && clearInterval(intervalId.current);
+
+    setInterval(treatTimer, 1000);
+
+    return () => {
+      intervalId.current && clearInterval(intervalId.current);
+    };
+  }, []);
+
+  const update = () => {
+    setTick((v) => v + 1);
   };
+
+  const treatTimer = () => {
+    if (gameState.current.status !== "inProgress") {
+      return;
+    }
+
+    if (gameState.current.timer > 0) {
+      gameState.current.timer--;
+      update();
+    }
+  };
+
+  const resetTimer = () => {
+    gameState.current.timer = 10;
+  };
+
+  const onClickChoice = (choice: string) => {
+    if (gameState.current.timer === 0) {
+      return;
+    }
+
+    const question =
+      gameState.current.questions?.[gameState.current.questionIndex];
+
+    if (
+      gameState.current.questionIndex <
+      gameState.current.questions.length - 1
+    ) {
+      gameState.current.questionIndex++;
+    } else {
+      gameState.current.status = "ended";
+    }
+
+    if (choice === question.answer) {
+      gameState.current.points += gameState.current.timer;
+    }
+
+    resetTimer();
+
+    update();
+  };
+
+  const onClickNext = () => {
+    if (
+      gameState.current.questionIndex <
+      gameState.current.questions.length - 1
+    ) {
+      gameState.current.questionIndex++;
+      resetTimer();
+    } else {
+      gameState.current.status = "ended";
+    }
+
+    update();
+  };
+
+  const onClickRetry = () => {
+    initializeGame();
+  };
+
+  const startGame = () => {
+    gameState.current.status = "inProgress";
+
+    update();
+  };
+
+  const { points, questionIndex, questions, status, timer } = gameState.current;
+
+  let messageForQuestionBlock = (
+    <>
+      Quando estiver pronto para começar clique em qualquer um dos botões abaixo
+      👇
+    </>
+  );
+
+  let messageForChoiceButton = <>Iniciar jogo</>;
+
+  let handlerForChoiceButtonClick = startGame;
+
+  if (status === "ended") {
+    messageForQuestionBlock = (
+      <>
+        O jogo anterior terminou 😁 <br />
+        Se quiser começar um novo jogo clique em qualquer um dos botões abaixo
+        👇
+      </>
+    );
+
+    messageForChoiceButton = <>Começar um novo jogo</>;
+
+    handlerForChoiceButtonClick = initializeGame;
+  }
+
+  const question = questions?.[questionIndex];
+
+  console.log("GameBlock", { gameState });
 
   return (
-    <div className="flex-col">
-      <div className="card min-w-80 w-auto bg-neutral-content p-8 text-white">
-        <span className="font-bold">Pergunta</span>
-      </div>
-      <div className="mb-8" />
-      <div className="card min-w-80 w-auto bg-neutral-content p-8 text-white">
-        <div className="grid grid-rows-2 grid-flow-col gap-4">
-          <div>
-            <button className="btn">resposta 1</button>
+    <>
+      {status === "error" ? (
+        <button className="btn btn-warning btn-lg" onClick={onClickRetry}>
+          <span className="text-lg">
+            Não foi possível iniciar o jogo!
+            <br /> Tente novamente
+          </span>
+        </button>
+      ) : (
+        <div className="flex-col">
+          <div className="card w-auto bg-neutral-content p-4 text-white flex flex-row justify-between items-center">
+            <div className="flex items-center">
+              <GiftIcon height={32} className="mr-4" />
+
+              <span className="font-bold text-4xl">{points}</span>
+            </div>
+
+            <div className="flex items-center">
+              <span className="font-bold text-4xl mr-4">{timer}</span>
+
+              <ClockIcon height={32} />
+            </div>
           </div>
-          <div>
-            <button className="btn">resposta 2</button>
+
+          <div className="mb-8" />
+
+          <div className="card min-w-150 w-auto bg-info p-8 text-white">
+            <QuestionMarkCircleIcon height={32} />
+
+            {status === "loading" ? (
+              <div className="skeleton mt-8 bg-gray-300 w-84 h-24"></div>
+            ) : ["readyToStart", "ended"].includes(status) ? (
+              <span className="mt-4 font-bold text-4xl">
+                {messageForQuestionBlock}
+              </span>
+            ) : (
+              <span className="mt-4 font-bold text-4xl">
+                {question?.question}
+              </span>
+            )}
           </div>
-          <div>
-            <button className="btn">resposta 3</button>
-          </div>
-          <div>
-            <button className="btn">resposta 4</button>
+
+          <div className="mb-8" />
+
+          <div className="card min-w-100 w-auto bg-neutral-content p-8 text-white">
+            <div
+              className={`grid grid-rows-2 grid-flow-col gap-4 z-10 ${
+                timer === 0 ? "opacity-50" : undefined
+              }`}
+            >
+              {status === "loading" ? (
+                [0, 1, 2, 3].map((x) => (
+                  <div key={x} className="skeleton bg-gray-300 w-48 h-24"></div>
+                ))
+              ) : ["readyToStart", "ended"].includes(status) ? (
+                [0, 1, 2, 3].map((x) => (
+                  <div key={x}>
+                    <button
+                      className="btn btn-accent w-48 h-24"
+                      onClick={handlerForChoiceButtonClick}
+                    >
+                      {messageForChoiceButton}
+                    </button>
+                  </div>
+                ))
+              ) : (
+                <>
+                  {question?.choices.map((choice) => (
+                    <div key={choice}>
+                      <button
+                        className="btn btn-accent w-48 h-24"
+                        onClick={() => onClickChoice(choice)}
+                        disabled={timer === 0}
+                      >
+                        {choice}
+                      </button>
+                    </div>
+                  ))}
+                </>
+              )}
+            </div>
+
+            {timer === 0 && (
+              <div className="absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button
+                  className="btn btn-primary w-48 h-24"
+                  onClick={onClickNext}
+                >
+                  Avançar
+                </button>
+              </div>
+            )}
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
--- src/pages/api/questions.ts
+++ src/pages/api/questions.ts
@@ -1,38 +1,139 @@
 import type { APIRoute } from "astro";
-import { openai } from "../../lib/openai";
 
-export const POST: APIRoute = async ({ request }) => {
-  const formData = await request.formData();
-  const age = formData.get("age");
-  const level = formData.get("level");
+const handler = async ({
+  age = "10 a 13",
+  language = "pt",
+  level = "3",
+}: {
+  age?: string;
+  level?: string;
+  language?: string;
+}) => {
+  // const messages = [
+  //   {
+  //     role: "user",
+  //     content: `É um jogo de quiz para crianças e adultos`,
+  //   },
+  //   {
+  //     role: "user",
+  //     content: `As perguntas tem 10 níveis de dificuldade sendo 1 o mais fácil e 10 o mais difícil`,
+  //   },
+  //   {
+  //     role: "user",
+  //     content: `A estrutura json de resposta deve ser no seguinte template: { questions: [{question: string; choices: string[]; awswer: string; }] }`,
+  //   },
+  //   {
+  //     role: "user",
+  //     content: `A linguagem utilizada deve ser ${language}`,
+  //   },
+  //   {
+  //     role: "user",
+  //     content: `Gera-me 10 perguntas e as respetivas 4 alternativas de respostas considerando a idade ${age} e o nível de dificuldade ${level} em json`,
+  //   },
+  // ] as any;
 
-  console.log("formData", { age, level });
+  // // console.log("formData", { age, level });
 
   try {
-    const aiRes = await openai.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: `É um jogo de quiz para crianças e adultos`,
-        },
-        {
-          role: "user",
-          content: `As perguntas tem 10 níveis de dificuldade sendo 1 o mais fácil e 10 o mais difícil`,
-        },
-        {
-          role: "user",
-          content: `Gera-me 10 perguntas e as respetivas respostas considerando a idade ${age} e o nível de dificuldade ${level} em json`,
-        },
-      ],
-      model: "gpt-3.5-turbo-1106",
-      response_format: { type: "json_object" },
-    });
+    // // const aiRes = await openai.chat.completions.create({
+    // //   messages: messages,
+    // //   model: "gpt-3.5-turbo-1106",
+    // //   response_format: { type: "json_object" },
+    // //   seed: Math.ceil(Math.random() * 100000000),
+    // // });
 
-    const questions = aiRes.choices[0].message.content;
+    // const questions: any = aiRes.choices[0].message.content;
 
-    console.log("game", { age, level, questions });
+    // // console.log("game", { age, level, questions });
 
-    return new Response(JSON.stringify({ status: "success", data: questions }));
+    // return new Response(
+    //   JSON.stringify({ status: "success", data: JSON.parse(questions) })
+    // );
+
+    return new Response(
+      JSON.stringify({
+        status: "success",
+        data: {
+          questions: [
+            {
+              question: "Qual é a capital do Brasil?",
+              choices: ["Buenos Aires", "Brasília", "Lima", "Rio de Janeiro"],
+              answer: "Brasília",
+            },
+            {
+              question: "Quantos planetas existem no nosso sistema solar?",
+              choices: ["8", "7", "9", "10"],
+              answer: "8",
+            },
+            {
+              question: "Quem pintou a Mona Lisa?",
+              choices: [
+                "Pablo Picasso",
+                "Vincent van Gogh",
+                "Leonardo da Vinci",
+                "Michelangelo",
+              ],
+              answer: "Leonardo da Vinci",
+            },
+            {
+              question: "Qual é o maior animal terrestre?",
+              choices: [
+                "Elefante africano",
+                "Baleia-azul",
+                "Girafa",
+                "Rinoceronte-branco",
+              ],
+              answer: "Elefante africano",
+            },
+            {
+              question: "Qual é a capital de Portugal?",
+              choices: ["Lisboa", "Porto", "Faro", "Coimbra"],
+              answer: "Lisboa",
+            },
+            {
+              question: "Quantos dias tem um ano bissexto?",
+              choices: ["365", "366", "364", "367"],
+              answer: "366",
+            },
+            {
+              question: "Quem escreveu a peça 'Romeu e Julieta'?",
+              choices: [
+                "William Shakespeare",
+                "Miguel de Cervantes",
+                "Jane Austen",
+                "Emily Brontë",
+              ],
+              answer: "William Shakespeare",
+            },
+            {
+              question: "Qual é o maior oceano do mundo?",
+              choices: [
+                "Oceano Atlântico",
+                "Oceano Índico",
+                "Oceano Ártico",
+                "Oceano Pacífico",
+              ],
+              answer: "Oceano Pacífico",
+            },
+            {
+              question: "Como se chama a estrela mais próxima da Terra?",
+              choices: [
+                "Alfa Centauri",
+                "Estrela de Barnard",
+                "Proxima Centauri",
+                "Vega",
+              ],
+              answer: "Proxima Centauri",
+            },
+            {
+              question: "Qual é a língua oficial do Brasil?",
+              choices: ["Espanhol", "Francês", "Inglês", "Português"],
+              answer: "Português",
+            },
+          ],
+        },
+      })
+    );
   } catch (err) {
     return new Response(
       JSON.stringify({
@@ -41,4 +142,21 @@ export const POST: APIRoute = async ({ request }) => {
       })
     );
   }
+};
+
+export const GET: APIRoute = async () => {
+  const age = "10 a 13";
+  const level = "3";
+  const language = "pt-pt";
+
+  return handler({ age, level, language });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  const formData = await request.formData();
+  const age = formData.get("age") as string;
+  const level = formData.get("level") as string;
+  const language = formData.get("language") as string;
+
+  return handler({ age, level, language });
 };
