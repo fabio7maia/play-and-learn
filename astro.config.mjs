a9a7bacea21d96e624fc2a79970a6fed30329900
Fábio Maia <fabio.maia@itsector.pt>
2023-12-24 02:20:42 +0000

    feat: refined site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://play-2-learn.netlify.app",
+  site: "https://play-2-learn.vercel.app",
   integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel(),
