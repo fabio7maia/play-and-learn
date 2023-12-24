22c4fb3efc466b21b53fde809c88980bb90e6166
Fábio Maia <fabio.maia@itsector.pt>
2023-12-24 02:20:20 +0000

    feat: moved from netlify to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://play-2-learn.netlify.app",
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
 });
